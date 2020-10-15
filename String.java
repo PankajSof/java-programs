be953f1e9d4776d36a945112ce47c8a294bd399f
Pawan Kumar <pk.soft29@gmail.com>
2020-10-15 13:13:59 +0530

    program to check given email is valid or not valid

--- String.java
+++ String.java
@@ -1,19 +1,45 @@
+package com.learning;
+
 import java.util.Scanner;
 
-public class EvenInRange {
+public class EmailCondition {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter lower limit");
-        int low = scanner.nextInt();
-        System.out.println("Enter last limit");
-        int max = scanner.nextInt();
-
-        for (int i = low; i <= max; i++)
-        {
-            if(i%2 == 0)
+    try {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your email id");
+        String name=sc.next();
+        int length = name.length();
+        int ind = name.indexOf("a");
+        boolean valid = false;
+        for (int i = 0; i < name.length(); i++) {
+            for (int j=0;j< name.length(); j++)
             {
-                System.out.println(i);
+                if (name.charAt(i) == '@')
+                {
+                    if (name.charAt(j)=='.')
+                    {
+                        valid=true;
+                    }
+
+                }
             }
+
+
         }
+        if (valid==true)
+        {
+            System.out.println("Valid email id");
+        }
+        else
+        {
+            System.out.println("invalid email id");
+        }
+    }
+    catch (Exception e)
+    {
+        System.out.println(e.getMessage());
+    }
+
+
     }
 }
