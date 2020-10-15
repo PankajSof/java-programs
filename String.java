e70384043c26ad8bcf48e496517286c298fcdde8
Pawan Kumar <pk.soft29@gmail.com>
2020-10-15 19:23:31 +0530

    Added: to check odd number

--- String.java
+++ String.java
@@ -1,36 +1,19 @@
-//WAP to get the array element through user using scanner class;
-//Print the size of array;
-//And print the occurance of element;
-//And search the element index number;
-//Print the index number;
 import java.util.Scanner;
 
-class LinearSearch {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+public class EvenInRange {
+public class OddInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter lower limit");
+@@ -10,7 +10,7 @@ public static void main(String[] args) {
 
-        System.out.println("Enter the size of array :");
-        int max=sc.nextInt();
-        int arr[] = new int[max];
-        for (int i=0; i< max; i++)
+        for (int i = low; i <= max; i++)
         {
-            System.out.println("Enter number");
-            int insertedDate= sc.nextInt();
-            arr[i]= insertedDate;
-        }
-        System.out.println("Size of array : "+arr.length);
-        System.out.println("Enter the number to search : ");
-        int numToSearch=sc.nextInt();
-        for (int i=0 ; i < arr.length; i++)
-        {
-            if(arr[i] == numToSearch)
+            if(i%2 == 0)
+            if(i%2 != 0)
             {
-                System.out.println(" We found the " + numToSearch + " at " +i+ " index ");
+                System.out.println(i);
             }
-
         }
-
-
     }
-
 }
